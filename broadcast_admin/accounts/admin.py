e4d8d99e61ec85b8987d35b94b0a41b21cdeea4b
Kim Do Ky <makingfunk0@gmail.com>
2018-10-31 00:33:45 +0900

    accounts / update admin

--- broadcast_admin/accounts/admin.py
+++ broadcast_admin/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import CustomUser, Team
 
-# Register your models here.
+admin.site.register(Team)
+
+class TeamInline(admin.TabularInline):
+    model = Team
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ['username', 'password', 'name', 'level', 'team', 'number', 'phone', 'birth', 'email']
+    inline = [
+        TeamInline,
+        ]
+
+admin.site.register(CustomUser, UserAdmin)
