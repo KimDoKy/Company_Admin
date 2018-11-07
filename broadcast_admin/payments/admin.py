e92d96b3d4e0da2a59da29e7a3a5b2b7596442a7
Kim Do Ky <makingfunk0@gmail.com>
2018-11-08 00:33:01 +0900

    payments/ fixed miss typo, admin registe

--- broadcast_admin/payments/admin.py
+++ broadcast_admin/payments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Payment)
+admin.site.register(PaymentComp)
+admin.site.register(PaymentMethod)
+admin.site.register(PayCard)
+admin.site.register(CardComp)
+admin.site.register(PayBank)
--- broadcast_admin/payments/models.py
+++ broadcast_admin/payments/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Payment(models.Model):
-    pmt_meney = models.CharField(max_length=14, verbose_name='결제금액')
+    pmt_money = models.CharField(max_length=14, verbose_name='결제금액')
     pmt_sell = models.IntegerField(blank=True, null=True, verbose_name='판매금액')
     pmt_tax = models.IntegerField(blank=True, null=True, verbose_name='부가세')
     pmt_comp = models.ForeignKey('PaymentComp', on_delete=models.PROTECT, verbose_name='승인회사')
@@ -10,7 +10,7 @@ class Payment(models.Model):
     # 결제금액을 입력하고 저장하면 자동으로 판매금액과 부가세를 계산해서 저장
     def save(self, *args, **kwargs):
         self.pmt_tax = int(self.pmt_money) / 11
-        self.pmg_sell = int(self.pmt_poney) - self.pmt_tax
+        self.pmt_sell = int(self.pmt_money) - self.pmt_tax
         super(Payment, self).save(*args, **kwargs)
 
     class Meta:
@@ -21,7 +21,7 @@ class Payment(models.Model):
 
 # 결제 승인회사
 class PaymentComp(models.Model):
-    company = models.CharField(max_length=10, unique=True, verbose_naem='승인회사')
+    company = models.CharField(max_length=10, unique=True, verbose_name='승인회사')
 
     def __str__(self):
         return f'{self.company}'
@@ -57,7 +57,7 @@ class PayCard(models.Model):
     card_sell = models.CharField(max_length=14, verbose_name='판매금액')
     card_comp = models.ForeignKey('CardComp', on_delete=models.PROTECT, verbose_name='카드사')
     card_num = models.CharField(max_length=20, verbose_name='카드번호')
-    card_sell_date = models.DateField(verbose_naem='결제일')
+    card_sell_date = models.DateField(verbose_name='결제일')
     card_life = models.CharField(max_length=8, verbose_name='카드유효기간')
     card_approval_num = models.CharField(max_length=10, verbose_name='승인번호')
     card_plan = models.IntegerField(verbose_name='할부개월수')
@@ -72,7 +72,7 @@ class PayCard(models.Model):
 
 # 카드사
 class CardComp(models.Model):
-    card_company = models.CharField(max_length=10, verbose_naem='카드사')
+    card_company = models.CharField(max_length=10, verbose_name='카드사')
 
     class Meta:
         verbose_name_plural = '카드사'
@@ -88,7 +88,7 @@ class PayBank(models.Model):
             )
     bank_money = models.CharField(max_length=14, verbose_name='입금금액')
     bank_name = models.CharField(max_length=10, choices=BANK_SELECT, verbose_name='입금은행')
-    bank_in_name = models.CharField(max_length=10, verbose_naem='입금자명')
+    bank_in_name = models.CharField(max_length=10, verbose_name='입금자명')
     bank_date = models.DateField(verbose_name='입금일')
 
     class Meta:
