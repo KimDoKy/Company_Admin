c0430d794378ac25f47df56763f890366e555726
Kim Do Ky <makingfunk0@gmail.com>
2018-11-07 12:50:02 +0900

    product / add verbose_name

--- broadcast_admin/product/models.py
+++ broadcast_admin/product/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class Product(models.Model):
-    prod_name = models.CharField(max_length=50, unique=True)
-    prod_code = models.AutoField(primary_key=True)
-    prod_act = models.BooleanField()
-    prod_memo = models.TextField(blank=True, null=True)
+    prod_name = models.CharField(max_length=50, unique=True, verbose_name='상품명')
+    prod_code = models.AutoField(primary_key=True, verbose_name='상품코드')
+    prod_act = models.BooleanField(verbose_name='사용여부')
+    prod_memo = models.TextField(blank=True, null=True, verbose_name='상품메모')
 
+    class Meta:
+        verbose_name_plural = '상품'
     def __str__(self):
         return f'{self.prod_name}'
