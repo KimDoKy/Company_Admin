5a8ae303558650a8f774db1fb4505cb51113c1dd
Kim Do Ky <makingfunk0@gmail.com>
2018-11-14 01:00:48 +0900

    accounts / remodeling

--- broadcast_admin/accounts/admin.py
+++ broadcast_admin/accounts/admin.py
@@ -1,15 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser, Team
 
-admin.site.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    model = Team
+    fields = ['team_name', 'team_deps']
 
 class TeamInline(admin.TabularInline):
     model = Team
 
 class UserAdmin(admin.ModelAdmin):
-    fields = ['username', 'password', 'name', 'level', 'team', 'number', 'phone', 'birth', 'email']
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['name', 'level', 'team']
+    fields = ['username', 'name', 'level', 'team', 'number', 'phone', 'birth', 'email']
     inline = [
         TeamInline,
         ]
 
 admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Team, TeamAdmin)
