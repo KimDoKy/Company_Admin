3f90db54b1e47c2fc731f757f9d443ecefc928eb
Kim Do Ky <makingfunk0@gmail.com>
2018-11-21 22:00:27 +0900

    viewser / settings login main logo, settings static

--- broadcast_admin/conf/settings.py
+++ broadcast_admin/conf/settings.py
@@ -122,5 +122,9 @@ USE_THOUSAND_SEPARATOR= True
 
 STATIC_URL = '/static/'
 
+STATICFIlES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    ]
+
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/accounts/login'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
--- broadcast_admin/conf/urls.py
+++ broadcast_admin/conf/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-]
+    path('', include('accounts.urls')),
+    path('', include('django.contrib.auth.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
