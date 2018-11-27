d39ec40bb47fe4cf4f5ce038d5db7b54c282a2ab
Kim Do Ky <makingfunk0@gmail.com>
2018-11-27 23:45:04 +0900

    abstractbaseuser / CustomUser 모델의 basic mothod 구현

--- broadcast_admin/accounts/urls.py
+++ broadcast_admin/accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView 
-from .views import SignUp, profile 
+# from .views import SignUp, profile
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html')),
-    path('signup/', SignUp.as_view(), name='signup'),
-    path('profile/', profile, name='profile'),
+    # path('signup/', SignUp.as_view(), name='signup'),
+    # path('profile/', profile, name='profile'),
     ]
--- broadcast_admin/accounts/views.py
+++ broadcast_admin/accounts/views.py
@@ -5,12 +5,12 @@ from django.views import generic
 from .models import CustomUser
 from .forms import CustomUserCreationForm
 
-class SignUp(generic.CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'signup.html'
-
-def profile(request):
-    username = request.user.get_username()
-    user = CustomUser.objects.get(username=username)
-    return render(request, 'profile.html', {'user':user})
+# class SignUp(generic.CreateView):
+#     form_class = CustomUserCreationForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'signup.html'
+#
+# def profile(request):
+#     username = request.user.get_username()
+#     user = CustomUser.objects.get(username=username)
+#     return render(request, 'profile.html', {'user':user})
